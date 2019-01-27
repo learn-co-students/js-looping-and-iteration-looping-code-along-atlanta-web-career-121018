799a1eddb17fc0ac1b0a66d218646a96b106bb75
Arthur Torres <arthurtorres002@gmail.com>
2019-01-27 14:12:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (var i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`)
+  }
+  return names
+}
+
+function tailsNeverFails() {
+  function coinFlipTails() {
+    return Math.random() >= 0.5
+  }
+
+  let tailsFlips = 0
+
+  while (coinFlipTails()) {
+    tailsFlips++
+  }
+
+  return `You got ${tailsFlips} tails in a row!`
+}
