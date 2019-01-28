b7d37ab7d4f4174f3fd5fbcecc3978e3ec91e0f4
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-01-28 08:30:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function printBadges(names) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Welcome ${names[i]}! You are employee #${[i+1]}.`);
+    }
+    return names;
+}
+
+function tailsNeverFails(){
+    tailsCount = 0;
+    coinFlip = Math.random();
+    while (coinFlip >= 0.5) {
+      tailsCount += 1;
+      coinFlip = Math.random();
+    }
+    return `You got ${tailsCount} tails in a row!`
+  }
